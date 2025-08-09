550d190be62b8ba03ccae576b2e1b221f4be52d9
Temps91 <jorgebd3045@gmail.com>
2025-08-09 17:56:28 -0600

    Subiendo MusicNotes y funcionamiento de flauta al igual de enemigos melodicos

---
+++ Assets/Devs/Flauta.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class Flauta : MonoBehaviour
+{
+    public bool tocandoFlauta;
+    public GameObject musicNotes;
+    public bool NotasMimic;
+
+    void Start()
+    {
+        tocandoFlauta = false;
+        musicNotes.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        FuncionFlauta();
+    }
+
+
+    public void FuncionFlauta()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && !tocandoFlauta && !NotasMimic)
+        {
+            tocandoFlauta = true;
+            NotasMimic = true;
+            musicNotes.SetActive(true);
+            Debug.Log("Estoy tocando la flauta");
+            StartCoroutine(FlautaActiva());
+            StartCoroutine(RecargaFlauta());
+
+        }
+
+    }
+
+    public IEnumerator FlautaActiva()
+    {
+
+        yield return new WaitForSeconds(2);
+        musicNotes.SetActive(false);
+        tocandoFlauta = false;
+        
+        Debug.Log("Deje de tocar la flauta");
+
+    }
+
+    public IEnumerator RecargaFlauta()
+    {
+        yield return new WaitForSeconds(7);
+        NotasMimic = false;
+    }
+}
--- Assets/Devs/Jorge/Scripts/MelodicoScript.cs
+++ Assets/Devs/Jorge/Scripts/MelodicoScript.cs
@@ -1,11 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
 public class MelodicoScript : MonoBehaviour
 {
     public float speed = 90f;
+    public Flauta flautascript;
+    public GameObject musicNotes;
+    private Coroutine respuestaCoroutine;
+    public bool Notas;
 
+
+    void Start()
+    {
+        Notas = false;
+        if (flautascript == null)
+        {
+            GameObject Player = GameObject.FindWithTag("Player");
+            if (Player != null)
+            {
+                flautascript = Player.GetComponent<Flauta>();
+            }
+        }
+        if (musicNotes == null)
+        {
+            musicNotes = transform.Find("MusicNotesParticles").gameObject;
+        }
+        musicNotes.SetActive(false);
+    }
     void Update()
     {
         transform.Rotate(0, speed * Time.deltaTime, 0);
+
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Jugador Collisionado Con Rango Melodico: " + other.name);
+
+        if (other.CompareTag("Player"))
+        {
+            if (flautascript.tocandoFlauta == true && flautascript.NotasMimic == true && !Notas)
+            {
+                Debug.Log("Jugador Toca Flauta");
+                musicNotes.SetActive(true);
+                StartCoroutine(Respuesta());
+                Notas = true;
+
+
+            }
+
+        }
+    }
+
+    IEnumerator Respuesta()
+    {
+        yield return new WaitForSeconds(2);
+        musicNotes.SetActive(false);
+        yield return new WaitForSeconds(5);
+        Notas = false;
+
     }
 }
