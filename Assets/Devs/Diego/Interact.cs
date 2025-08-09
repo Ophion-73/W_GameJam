4b5f2e1403ec69863a63ae95468b834ed6c91d3e
Ophion-73 <cdmx2754@amerike.edu.mx>
2025-08-08 21:56:10 -0600

    avance 1

---
+++ Assets/Devs/Diego/Interact.cs
@@ -0,0 +1,111 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    [Header("General Behaviour")]
+    public bool isInteracting;
+    Ray ray;
+    [Header("Interact Behaviour")]
+    
+    public float rayLengthInteract;
+    public Transform originalCameraPos;
+    public Transform cameraPos;
+    public LayerMask interactableObj;
+    public KeyCode interactKey;
+    
+    
+
+    [Header("Rotation Behaviour")]
+    
+    public float rayLengthMove;
+    public float minRot;
+    public float maxRot;
+    public GameObject staticPart;
+    public GameObject movablePart;
+    public GameObject playerCamera;
+    public Transform newCameraPos;
+    public LayerMask movableObj;
+
+    [Header("public mouse pos")]
+    public Vector2 clickPos;
+    public Vector2 continousMousePos;
+    public float xDiff;
+    public float yDiff;
+    bool canClick;
+    
+
+    private void Start()
+    {
+        isInteracting = false;
+        canClick = false;
+    }
+
+    private void Update()
+    {
+        float raylength;
+       if(!isInteracting)
+        {
+            raylength = rayLengthInteract;
+            ray = new Ray(cameraPos.position, cameraPos.forward);
+            Debug.DrawRay(ray.origin, ray.direction * rayLengthInteract, Color.red);
+            if (Physics.Raycast(ray , out RaycastHit hit, raylength, interactableObj))
+            {
+                if(Input.GetKeyDown(interactKey))
+                {
+                    InteractWithObject(hit.collider.gameObject);
+                    isInteracting=true;
+                }
+            }
+        }
+       else
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                GetComponent<Camera>().transform.position = originalCameraPos.position;
+                GetComponent<Camera>().transform.rotation = originalCameraPos.rotation;
+                isInteracting = false;
+            }
+            
+            raylength = rayLengthMove;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * rayLengthInteract, Color.red);
+            
+            if (Physics.Raycast(ray, out RaycastHit hit, raylength, movableObj))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    clickPos = Input.mousePosition;
+                    Debug.Log("objetomovible");
+                    canClick = true;
+                    movablePart = hit.collider.gameObject;
+                }
+            }
+            if (Input.GetMouseButton(0) && canClick)
+            {
+                continousMousePos = Input.mousePosition;
+                xDiff = continousMousePos.x - clickPos.x;
+                yDiff = continousMousePos.y - clickPos.y;
+            }
+            if (Input.GetMouseButtonUp(0) && canClick)
+            {
+                clickPos = new Vector2(0, 0);
+                continousMousePos = new Vector2(0, 0);
+                xDiff = 0;
+                yDiff = 0;
+                movablePart = null;
+                canClick = false;
+            }
+        }
+
+       
+    }
+
+    public void InteractWithObject(GameObject target)
+    {
+        GetComponent<Camera>().transform.position = newCameraPos.position;
+        GetComponent<Camera>().transform.rotation = newCameraPos.rotation;
+
+
+    }
+}
