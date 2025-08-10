c466613add0b6857ac948370f06bb71bc52e1618
Ophion-73 <cdmx2754@amerike.edu.mx>
2025-08-09 22:59:50 -0600

    inventory Manager

---
+++ Assets/Devs/Diego/InventoryManageer.cs
@@ -0,0 +1,113 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InventoryManageer : MonoBehaviour
+{
+    public int itemIndex;
+    public GameObject[] items;
+    [Header("General")]
+    public int puntuacion;
+
+    [Header("Detector de mimics")]
+    public int monsterQ;
+    public Material[] numTex;
+
+
+    [Header("Cuchillo")]
+    public Transform camera;
+    public float rayLength;
+    public LayerMask objects;
+    public string objectTag;
+    public string mimicTag;
+
+    private void Start()
+    {
+        items[0].GetComponent<MeshRenderer>().material = numTex[0];
+    }
+    private void Update()
+    {
+        UseItem();
+    }
+
+    public void ChangeActiveItem(int index)
+    {
+        itemIndex = index;
+        items[itemIndex + 1].SetActive(false);
+        items[itemIndex - 1].SetActive(false);
+        items[itemIndex].SetActive(true);
+        
+    }
+    public void UseItem()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            switch(itemIndex)
+            {
+                //Detector de numero de mimics
+                case 0:
+
+                    UseMimicDetector();
+                    break;
+                //Cuchillo
+                case 1:
+                    UseKnife();
+                    break;
+                case 2:
+                    UseMeat();
+                    break;
+                case 3:
+                    UseCandle();
+                    break;
+
+                case 4:
+                    UseFlute();
+                    break;
+
+            }
+        }
+    }
+    public void UseFlute()
+    {
+        //funcionalidad de flauta
+    }
+    private void UseCandle()
+    {
+        if (Physics.Raycast(new Ray(camera.position, camera.forward), out RaycastHit hit, rayLength, objects))
+        {
+            GameObject hitGameObject = hit.collider.gameObject;
+            if (hitGameObject.CompareTag(mimicTag) && hitGameObject.GetComponent<ShadowScript>() != null)
+            {
+                //hitGameObject.GetComponent<ShadowScript>()."aqui va el metodo para dacudir a los bros";
+            }
+        }
+    }
+
+    private void UseMeat()
+    {
+        //combinar con el de jorge
+    }
+
+    public void UseKnife()
+    {
+        if(Physics.Raycast(new Ray(camera.position,camera.forward), out RaycastHit hit,  rayLength, objects))
+        {
+            GameObject hitGameObject = hit.collider.gameObject;
+            if (hitGameObject.CompareTag(mimicTag))
+            {
+                hitGameObject.SetActive(false);
+                Debug.Log("Mataste un mmimic");
+            }
+            if(hitGameObject.CompareTag(objectTag))
+            {
+                hitGameObject.SetActive(false);
+                Debug.Log("Lacagaste");
+            }
+        }
+    }
+
+    public void UseMimicDetector()
+    {
+        items[0].GetComponent<MeshRenderer>().material = numTex[monsterQ];
+    }
+}
